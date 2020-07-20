fae3ebe3759535e29d7d1d459af9d6a8e0d0df9e
nandan-unni <asnqln@gmail.com>
2020-07-20 23:48:21 +0530

    Getting ready for deployment

--- key_blogs/settings.py
+++ key_blogs/settings.py
@@ -5,6 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_ROOT = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
